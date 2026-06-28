16e439bdb8bbcc6996c5cae45dc5b0efa35f8e80
Jim Liu 宝玉 <junminliu@gmail.com>
2026-06-28 08:16:06 -0500

    local-remote-dropdown: align raw aliases

--- restored/boundaries/current-ref/local-remote-dropdown-producer.ts
+++ restored/boundaries/current-ref/local-remote-dropdown-producer.ts
@@ -1,19 +1,17 @@
 // Restored from ref/webview/assets/local-remote-dropdown-BcmhHtfg.js
 export {
+  CloudEnvironmentDropdown,
+  CloudEnvironmentDropdown as a,
+  isComposerModeVisibleInLocalRemoteDropdown as c,
+  initCloudEnvironmentDropdownChunk,
+  initCloudEnvironmentDropdownChunk as o,
+  initLocalRemoteDropdownChunk,
+  initLocalRemoteDropdownChunk as n,
+  initLocalRemoteDropdownStateChunk as s,
   initThreadHandoffSummaryHelpersChunk,
   initThreadHandoffSummaryHelpersChunk as i,
+  LocalRemoteDropdown,
+  LocalRemoteDropdown as t,
   shouldShowThreadHandoffInSummary as r,
   shouldShowThreadHandoffInSummary,
 } from "../../thread-summary/local-remote-dropdown";
-export {
-  a,
-  c,
-  CloudEnvironmentDropdown,
-  initCloudEnvironmentDropdownChunk,
-  initLocalRemoteDropdownChunk,
-  LocalRemoteDropdown,
-  n,
-  o,
-  s,
-  t,
-} from "../../vendor/local-remote-dropdown-runtime";
