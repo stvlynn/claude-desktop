e027d66dd6d4d5da2f20489f4cd3d1150a1f3f32
Jim Liu 宝玉 <junminliu@gmail.com>
2026-06-29 11:56:31 -0500

    main: restore local plugin migration helpers

---
+++ restored/main/plugins/local-to-bundled-migration/index.ts
@@ -0,0 +1,11 @@
+// Restored from ref/.vite/build/main-r5HnecX_.js
+// Barrel for local plugin to bundled plugin migrations.
+
+export * from "./browser-use";
+export * from "./computer-use";
+export * from "./config-cleanup";
+export * from "./constants";
+export * from "./marketplace";
+export * from "./marketplace-cleanup";
+export * from "./paths";
+export * from "./types";
