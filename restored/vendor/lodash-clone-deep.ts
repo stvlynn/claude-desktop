67349fd43c1e311895d563281b005a6970084d37
Jim Liu 宝玉 <junminliu@gmail.com>
2026-06-30 08:35:02 -0500

    type restored lodash clone helpers

--- restored/vendor/lodash-clone-deep.ts
+++ restored/vendor/lodash-clone-deep.ts
@@ -1,6 +1,6 @@
 // Restored from ref/webview/assets/clone-2yQp58UZ.js
 // Clone chunk restored from the Codex webview bundle.
 import { unionR } from "./lodash-union";
-export function clone(_clone) {
-  return unionR(_clone, 4);
+export function clone(value: unknown): unknown {
+  return unionR(value, 4);
 }
--- restored/vendor/lodash-clone.ts
+++ restored/vendor/lodash-clone.ts
@@ -1,6 +1,6 @@
 // Restored from ref/webview/assets/clone-Mnyt6qd1.js
 // Clone chunk restored from the Codex webview bundle.
 import { baseUniqY } from "./lodash-base-uniq";
-export function clone(_clone) {
-  return baseUniqY(_clone, 4);
+export function clone(value: unknown): unknown {
+  return baseUniqY(value, 4);
 }
