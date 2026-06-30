1591e47952bf23f157fd433ee4b338ea0a0620f7
Jim Liu 宝玉 <junminliu@gmail.com>
2026-06-30 11:23:59 -0500

    restore d3 monotone curve helpers

--- restored/vendor/d3-curve-monotone.ts
+++ restored/vendor/d3-curve-monotone.ts
@@ -1,213 +1,180 @@
 // Restored from ref/webview/assets/monotone-DKOUTWfZ.js
-// Monotone chunk restored from the Codex webview bundle.
-function monotoneHelper1(monotoneParam17) {
-  this._context = monotoneParam17;
+// d3-shape linear and monotone curve factories restored from the Codex webview bundle.
+import type { Curve, CurveContext } from "./d3-shape-curve-context";
+import { curveLinear } from "./d3-shape-curve-linear";
+export { curveLinear } from "./d3-shape-curve-linear";
+
+interface MonotoneState {
+  _context: CurveContext;
+  _x0: number;
+  _x1: number;
+  _y0: number;
+  _y1: number;
+  _t0: number;
 }
-monotoneHelper1.prototype = {
-  areaStart: function () {
-    this._line = 0;
-  },
-  areaEnd: function () {
-    this._line = NaN;
-  },
-  lineStart: function () {
-    this._point = 0;
-  },
-  lineEnd: function () {
-    (this._line || (this._line !== 0 && this._point === 1)) &&
-      this._context.closePath();
-    this._line = 1 - this._line;
-  },
-  point: function (monotoneParam2, _monotoneR) {
-    switch (
-      ((monotoneParam2 = +monotoneParam2),
-      (_monotoneR = +_monotoneR),
-      this._point)
-    ) {
-      case 0:
-        this._point = 1;
-        this._line
-          ? this._context.lineTo(monotoneParam2, _monotoneR)
-          : this._context.moveTo(monotoneParam2, _monotoneR);
-        break;
-      case 1:
-        this._point = 2;
-      default:
-        this._context.lineTo(monotoneParam2, _monotoneR);
-        break;
-    }
-  },
-};
-export function monotoneR(_monotoneR) {
-  return new monotoneHelper1(_monotoneR);
+
+function sign(value: number): -1 | 1 {
+  return value < 0 ? -1 : 1;
 }
-function monotoneHelper2(monotoneParam18) {
-  return monotoneParam18 < 0 ? -1 : 1;
-}
-function monotoneHelper3(monotoneParam3, _monotoneR, monotoneParam4) {
-  var monotoneValue2 = monotoneParam3._x1 - monotoneParam3._x0,
-    monotoneValue3 = _monotoneR - monotoneParam3._x1,
-    monotoneValue4 =
-      (monotoneParam3._y1 - monotoneParam3._y0) /
-      (monotoneValue2 || (monotoneValue3 < 0 && 0)),
-    monotoneValue5 =
-      (monotoneParam4 - monotoneParam3._y1) /
-      (monotoneValue3 || (monotoneValue2 < 0 && 0)),
-    monotoneValue6 =
-      (monotoneValue4 * monotoneValue3 + monotoneValue5 * monotoneValue2) /
-      (monotoneValue2 + monotoneValue3);
+
+function slope3(curve: MonotoneState, x: number, y: number): number {
+  const h0 = curve._x1 - curve._x0;
+  const h1 = x - curve._x1;
+  const s0 = (curve._y1 - curve._y0) / (h0 || (h1 < 0 ? -0 : 0));
+  const s1 = (y - curve._y1) / (h1 || (h0 < 0 ? -0 : 0));
+  const p = (s0 * h1 + s1 * h0) / (h0 + h1);
+
   return (
-    (monotoneHelper2(monotoneValue4) + monotoneHelper2(monotoneValue5)) *
-      Math.min(
-        Math.abs(monotoneValue4),
-        Math.abs(monotoneValue5),
-        0.5 * Math.abs(monotoneValue6),
-      ) || 0
+    (sign(s0) + sign(s1)) *
+      Math.min(Math.abs(s0), Math.abs(s1), 0.5 * Math.abs(p)) || 0
   );
 }
-function monotoneHelper4(monotoneParam7, _monotoneR) {
-  var monotoneValue12 = monotoneParam7._x1 - monotoneParam7._x0;
-  return monotoneValue12
-    ? ((3 * (monotoneParam7._y1 - monotoneParam7._y0)) / monotoneValue12 -
-        _monotoneR) /
-        2
-    : _monotoneR;
+
+function slope2(curve: MonotoneState, t: number): number {
+  const h = curve._x1 - curve._x0;
+  return h ? ((3 * (curve._y1 - curve._y0)) / h - t) / 2 : t;
 }
-function monotoneHelper5(monotoneParam5, _monotoneR, monotoneParam6) {
-  var monotoneValue7 = monotoneParam5._x0,
-    monotoneValue8 = monotoneParam5._y0,
-    monotoneValue9 = monotoneParam5._x1,
-    monotoneValue10 = monotoneParam5._y1,
-    monotoneValue11 = (monotoneValue9 - monotoneValue7) / 3;
-  monotoneParam5._context.bezierCurveTo(
-    monotoneValue7 + monotoneValue11,
-    monotoneValue8 + monotoneValue11 * _monotoneR,
-    monotoneValue9 - monotoneValue11,
-    monotoneValue10 - monotoneValue11 * monotoneParam6,
-    monotoneValue9,
-    monotoneValue10,
+
+function point(curve: MonotoneState, t0: number, t1: number): void {
+  const x0 = curve._x0;
+  const y0 = curve._y0;
+  const x1 = curve._x1;
+  const y1 = curve._y1;
+  const dx = (x1 - x0) / 3;
+
+  curve._context.bezierCurveTo(
+    x0 + dx,
+    y0 + dx * t0,
+    x1 - dx,
+    y1 - dx * t1,
+    x1,
+    y1,
   );
 }
-function monotoneHelper6(monotoneParam19) {
-  this._context = monotoneParam19;
-}
-monotoneHelper6.prototype = {
-  areaStart: function () {
+
+class MonotoneX implements Curve, MonotoneState {
+  readonly _context: CurveContext;
+  _x0 = NaN;
+  _x1 = NaN;
+  _y0 = NaN;
+  _y1 = NaN;
+  _t0 = NaN;
+  private _line = NaN;
+  private _point = 0;
+
+  constructor(context: CurveContext) {
+    this._context = context;
+  }
+
+  areaStart(): void {
     this._line = 0;
-  },
-  areaEnd: function () {
+  }
+
+  areaEnd(): void {
     this._line = NaN;
-  },
-  lineStart: function () {
+  }
+
+  lineStart(): void {
     this._x0 = this._x1 = this._y0 = this._y1 = this._t0 = NaN;
     this._point = 0;
-  },
-  lineEnd: function () {
+  }
+
+  lineEnd(): void {
     switch (this._point) {
       case 2:
         this._context.lineTo(this._x1, this._y1);
         break;
       case 3:
-        monotoneHelper5(this, this._t0, monotoneHelper4(this, this._t0));
+        point(this, this._t0, slope2(this, this._t0));
         break;
     }
-    (this._line || (this._line !== 0 && this._point === 1)) &&
+    if (this._line || (this._line !== 0 && this._point === 1)) {
       this._context.closePath();
-    this._line = 1 - this._line;
-  },
-  point: function (monotoneParam1, _monotoneR) {
-    var monotoneValue1 = NaN;
-    if (
-      ((monotoneParam1 = +monotoneParam1),
-      (_monotoneR = +_monotoneR),
-      !(monotoneParam1 === this._x1 && _monotoneR === this._y1))
-    ) {
-      switch (this._point) {
-        case 0:
-          this._point = 1;
-          this._line
-            ? this._context.lineTo(monotoneParam1, _monotoneR)
-            : this._context.moveTo(monotoneParam1, _monotoneR);
-          break;
-        case 1:
-          this._point = 2;
-          break;
-        case 2:
-          this._point = 3;
-          monotoneHelper5(
-            this,
-            monotoneHelper4(
-              this,
-              (monotoneValue1 = monotoneHelper3(
-                this,
-                monotoneParam1,
-                _monotoneR,
-              )),
-            ),
-            monotoneValue1,
-          );
-          break;
-        default:
-          monotoneHelper5(
-            this,
-            this._t0,
-            (monotoneValue1 = monotoneHelper3(
-              this,
-              monotoneParam1,
-              _monotoneR,
-            )),
-          );
-          break;
-      }
-      this._x0 = this._x1;
-      this._x1 = monotoneParam1;
-      this._y0 = this._y1;
-      this._y1 = _monotoneR;
-      this._t0 = monotoneValue1;
     }
-  },
-};
-function monotoneHelper7(monotoneParam16) {
-  this._context = new monotoneHelper8(monotoneParam16);
+    this._line = 1 - this._line;
+  }
+
+  point(x: number, y: number): void {
+    let t1 = NaN;
+    x = +x;
+    y = +y;
+
+    if (x === this._x1 && y === this._y1) return;
+
+    switch (this._point) {
+      case 0:
+        this._point = 1;
+        this._line ? this._context.lineTo(x, y) : this._context.moveTo(x, y);
+        break;
+      case 1:
+        this._point = 2;
+        break;
+      case 2:
+        this._point = 3;
+        point(this, slope2(this, (t1 = slope3(this, x, y))), t1);
+        break;
+      default:
+        point(this, this._t0, (t1 = slope3(this, x, y)));
+        break;
+    }
+
+    this._x0 = this._x1;
+    this._x1 = x;
+    this._y0 = this._y1;
+    this._y1 = y;
+    this._t0 = t1;
+  }
 }
-(monotoneHelper7.prototype = Object.create(monotoneHelper6.prototype)).point =
-  function (monotoneParam13, _monotoneR) {
-    monotoneHelper6.prototype.point.call(this, _monotoneR, monotoneParam13);
-  };
-function monotoneHelper8(monotoneParam20) {
-  this._context = monotoneParam20;
+
+class MonotoneY extends MonotoneX {
+  constructor(context: CurveContext) {
+    super(new ReflectContext(context));
+  }
+
+  override point(x: number, y: number): void {
+    super.point(y, x);
+  }
 }
-monotoneHelper8.prototype = {
-  moveTo: function (monotoneParam14, _monotoneR) {
-    this._context.moveTo(_monotoneR, monotoneParam14);
-  },
-  closePath: function () {
+
+class ReflectContext implements CurveContext {
+  private readonly _context: CurveContext;
+
+  constructor(context: CurveContext) {
+    this._context = context;
+  }
+
+  moveTo(x: number, y: number): void {
+    this._context.moveTo(y, x);
+  }
+
+  lineTo(x: number, y: number): void {
+    this._context.lineTo(y, x);
+  }
+
+  closePath(): void {
     this._context.closePath();
-  },
-  lineTo: function (monotoneParam15, _monotoneR) {
-    this._context.lineTo(_monotoneR, monotoneParam15);
-  },
-  bezierCurveTo: function (
-    monotoneParam8,
-    _monotoneR,
-    monotoneParam9,
-    monotoneParam10,
-    monotoneParam11,
-    monotoneParam12,
-  ) {
-    this._context.bezierCurveTo(
-      _monotoneR,
-      monotoneParam8,
-      monotoneParam10,
-      monotoneParam9,
-      monotoneParam12,
-      monotoneParam11,
-    );
-  },
-};
-export function monotoneT(monotoneParam21) {
-  return new monotoneHelper6(monotoneParam21);
+  }
+
+  bezierCurveTo(
+    cx1: number,
+    cy1: number,
+    cx2: number,
+    cy2: number,
+    x: number,
+    y: number,
+  ): void {
+    this._context.bezierCurveTo(cy1, cx1, cy2, cx2, y, x);
+  }
 }
-export function monotoneN(monotoneParam22) {
-  return new monotoneHelper7(monotoneParam22);
+
+export function curveMonotoneX(context: CurveContext): Curve {
+  return new MonotoneX(context);
 }
+
+export function curveMonotoneY(context: CurveContext): Curve {
+  return new MonotoneY(context);
+}
+
+export const monotoneR = curveLinear;
+export const monotoneT = curveMonotoneX;
+export const monotoneN = curveMonotoneY;
